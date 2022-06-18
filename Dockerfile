8bd22477f0a3492f655cb018a3fd0e4201248f2d
DmitriyMelanich <melanich.dmitriy@gmail.com>
2022-06-18 12:13:41 +0300

    init

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,6 @@
 FROM openjdk:14
 EXPOSE 8080
-ADD build/libs/ExchangeRates-0.0.1.jar ExchangeRates-0.0.1.jar
-ENTRYPOINT ["java", "-jar", "ExchangeRates-0.0.1.jar"]
+COPY . /app
+WORKDIR /app
+RUN ./gradlew build
+ENTRYPOINT ["java", "-jar", "build/libs/ExchangeRates-0.0.1.jar"]
--- src/main/java/com/melanich/ExchangeRates/controller/MainController.java
+++ src/main/java/com/melanich/ExchangeRates/controller/MainController.java
@@ -9,7 +9,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import java.util.Map;
 
 @RestController
 @RequestMapping()
